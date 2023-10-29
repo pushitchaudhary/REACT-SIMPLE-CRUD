437f8035a012c2d509c74fad46792a00b16bbe7c
pushitchaudhary <pushitcoc@gmail.com>
2023-10-29 17:23:18 +0545

    show blogs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,42 +1,50 @@
-import Navbar from '../Component/Navbar/Navbar'
-import './Home.css'
-import axios from 'axios'
-import {useEffect, useState} from 'react'
-
+import Navbar from "../Component/Navbar/Navbar";
+import "./Home.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [blogs, setBlogs] = useState([]);
 
-   const [ blog, setBlog ] = useState([])
+  const fetchBlog = async () => {
+    const response = await axios.get("http://localhost:4000/blogs");
+    console.log(response);
+    setBlogs(response.data.blogs)
+  };
 
-   const fetchBlog = async ()=>{
-      const response = await axios.get('http://localhost:4000/blogs')
-      console.log(response)
-     setBlog[response]
-   }
-
-   useEffect(()=>{
-      fetchBlog()
-   },[])
-
+  useEffect(() => {
+    fetchBlog();
+  }, []);
 
   return (
-   <> 
-   <header>
-        <h1>Stylish Blog</h1>
-    </header>
-    <Navbar/>
-   <main>
-   <article>
-      this is artical
-   </article>
-   
-</main>
+    <>
+      <Navbar />
+      <main>
 
-<footer>
-   <p>&copy; 2023 Stylish Blog</p>
-</footer>
-   </>
-  )
+         {blogs.map((blog)=>{
+           return(
+            <article key={blog._id}>
+            <h2>{blog.title}</h2>
+            <h3>{blog.subTitle}</h3>
+            <p>
+              {blog.description}
+            </p>
+            <a href="#" className="read-more">
+              Read More
+            </a>
+          </article>
+           )
+         })}
+        
+     
+
+      </main>
+
+      <footer>
+        <p>&copy; 2023 Stylish Blog</p>
+      </footer>
+    </>
+  );
 }
 
-export default Home
+export default Home;
