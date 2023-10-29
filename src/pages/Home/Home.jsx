ed0d0794d9c8b214060ab9655ac21da3b569e573
pushitchaudhary <pushitcoc@gmail.com>
2023-10-29 16:20:46 +0545

    data fetch from backend

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
 import Navbar from '../Component/Navbar/Navbar'
 import './Home.css'
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+
+
 function Home() {
+
+   const [ blog, setBlog ] = useState([])
+
+   const fetchBlog = async ()=>{
+      const response = await axios.get('http://localhost:4000/blogs')
+      console.log(response)
+     setBlog[response]
+   }
+
+   useEffect(()=>{
+      fetchBlog()
+   },[])
+
+
   return (
    <> 
    <header>
@@ -10,10 +27,7 @@ function Home() {
     <Navbar/>
    <main>
    <article>
-       <h2>Blog Post Title</h2>
-       <p class="post-meta">Posted by <span class="author">Author</span> on <time datetime="2023-10-29">October 29, 2023</time> in <a href="#">Category</a></p>
-       <p>This is the main content of your blog post. Write an engaging and informative article here.</p>
-       <a href="#" class="read-more">Read More</a>
+      this is artical
    </article>
    
 </main>
